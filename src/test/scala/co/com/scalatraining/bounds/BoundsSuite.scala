cd63d4527a6090a666401489b973f0d84b7382eb
juanpavergara <vergaravillarraga@gmail.com>
2017-01-02 08:25:20 -0500

    La suite de bounds esta incompleta

--- src/test/scala/co/com/scalatraining/bounds/BoundsSuite.scala
+++ src/test/scala/co/com/scalatraining/bounds/BoundsSuite.scala
@@ -3,6 +3,11 @@ package co.com.scalatraining.bounds
 import org.scalatest.FunSuite
 import sun.reflect.generics.reflectiveObjects.NotImplementedException
 
+/*
+Esta suite esta incompleta y debe ser completada por los autores para dar guia en el estudio
+de lower y upper bounds
+ */
+
 class BoundsSuite extends FunSuite{
 
   test("Lower bound"){
